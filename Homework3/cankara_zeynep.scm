5ceab53af492a5fab582df811deacc813eef9995
zeynepCankara <zeynepcankara402@gmail.com>
2020-05-14 19:54:50 +0300

    CS315 HWs finished :elephant:

---
+++ Homework3/cankara_zeynep.scm
@@ -0,0 +1,29 @@
+#|
+Course: CS315
+Section: 01
+Author: Zeynep Cankara
+Id: 21703381
+|#
+
+;;; f,  f(k) =(k+1)^2
+(define (f k)
+      (+ (+ (* 2 k)(* k k)) 1))
+
+;;; sum-of-f:  sum-of-f(k) = f(k) + sum-of-f(k-1)
+(define (sum-of-f k)
+  (if (< k 1) (f k) (+ (f k)
+                   (sum-of-f (- k 1)) )))
+
+ ;;; sum-of-f-helper, helper function of tail recursive function f 
+(define (sum-of-f-helper k partial)
+                      (if (< k 1)
+                         partial
+                         sum-of-f-helper((- k 1) (+ k partial)))
+                    )
+                
+ ;;; sum-of-f-tr, tail recursive version of sum-of-f
+(define (sum-of-f-tr k)
+                  (sum-of-f-helper k (f 0)))
+
+
+ 
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Programming_Languages_HWs
+# **Programming Languages (CS315) Homeworks** 
+
 This repository contains my solutions to the Bilkent University CS315 Homework Assignments. (Spring 2020)
+
+## **Assignments** :ledger:
+
+- `Homework 1:` Arrays in Dart, Javascript, PHP, Python, and Rust
+- `Homework 2:` Counter-Controlled Loops in Dart, Javascript, PHP, Python, and Rust
+- `Homework 3:` Recursive Functions in Scheme
+ 
